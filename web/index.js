163c5ae210f3573998baceff067ce2eaa0bafb18
Pavelas Morozovas <pmorozovas@instarem.com>
2019-09-09 17:07:06 +0300

    Add a health-check route

--- web/index.js
+++ web/index.js
@@ -2,11 +2,18 @@ const { ipAuthentication, authorization, basic } = require('../middleware');
 const router = require('express').Router();
 
 basic(router);
+
+// Insecure routes
+require('./healthcheck')(router);
+
 ipAuthentication(router);
 authorization(router);
+
 // secured components
 require('./rates')(router);
 require('./reuterRates')(router);
+
+
 
 /**
  * Mounting respective paths.
