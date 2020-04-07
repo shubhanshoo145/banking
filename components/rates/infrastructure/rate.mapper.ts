bd6d775c1a0785b2fbe6e9e15f21ed19e4ab5567
Pavelas Morozovas <pmorozovas@instarem.com>
2020-04-07 08:51:45 +0300

    Move setting of the expiry time to the mapper

--- components/rates/infrastructure/rate.mapper.ts
+++ components/rates/infrastructure/rate.mapper.ts
@@ -1,14 +1,25 @@
+import * as moment from 'moment';
+import { IConfig } from 'config';
+
+import types from '../../../constants/types';
+import container from '../../../container';
 import { IRate } from "../../../commons/interfaces/entities/IRate";
 import { IReutersResponseItem } from "../rate.interfaces";
 import { ICurrencyPair } from "../../../commons/interfaces/entities/ICurrencyPair";
 import { Rate } from "../entities/rate";
+import { IApplicationConfig } from '../../../commons/interfaces/config/IApplicationConfig';
 
 export class RateMapper {
   public static toDomain(raw: IReutersResponseItem, currencyPair: ICurrencyPair): IRate {
+    const applicationConfig: IApplicationConfig = (
+      container.get<IConfig>(types.Config)
+    ).get('default.application');
+
     return Rate.create({
       currencyPair,
       fxRate: RateMapper.getCalculatedRate(raw, currencyPair),
       scalingFactor: Number.parseFloat(raw.Fields?.Field?.find(field => field.Name === 'SCALING')?.Utf8String) || 1,
+      expiresAt: moment.utc().add(applicationConfig.EXPIRY_MINUTES, 'm').toDate(),
     });
   }
 
--- web/endpoints/cronjobs.controller.ts
+++ web/endpoints/cronjobs.controller.ts
@@ -14,10 +14,8 @@ export default (router: Router) => {
     try {
     const currencyService = container.get<ICurrencyService>(types.CurrencyService);
     const rateService = container.get<IRateService>(types.RateService);
-    const config = container.get<IConfig>(types.Config);
 
     const currencies = await currencyService.getCurrencies();
-
     const currencyPairs: ICurrencyPair[] = [];
     currencies.forEach((sourceCurrency) => {
       currencies.forEach((destinationCurrency) => {
@@ -27,11 +25,6 @@ export default (router: Router) => {
       });
     });
     const rates = await rateService.getRates(currencyPairs);
-
-    // TODO: This seems very out of place
-    rates.forEach((rate) => {
-      rate.expiresAt = moment.utc().add(config.get('default.application.EXPIRY_MINUTES'), 'm').toDate();
-    });
 
     await Promise.all([
       rateService.storeRates(rates),
