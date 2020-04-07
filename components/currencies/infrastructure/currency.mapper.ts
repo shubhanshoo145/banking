d0c23d995ad0652027436804726bc52d75504f9c
Pavelas Morozovas <pmorozovas@instarem.com>
2020-04-07 12:41:48 +0300

    Simplify the currency mapper

--- components/currencies/infrastructure/currency.mapper.ts
+++ components/currencies/infrastructure/currency.mapper.ts
@@ -5,14 +5,11 @@ import { Currency } from "../entities/currency";
 export class CurrencyMapper {
   public static toDomain(raw: ICurrencyDocument): ICurrency;
   public static toDomain(raw: ILeanCurrencyDocument): ICurrency;
-  public static toDomain(raw: any): ICurrency {
-    const leanDocument: ILeanCurrencyDocument = raw.toObject ?
-      raw.toObject() : raw;
-    
+  public static toDomain(raw: ICurrencyDocument | ILeanCurrencyDocument): ICurrency {   
     return Currency.create({
-      isoCode: leanDocument.currency_label,
-      name: leanDocument.currency_name,
-      active: leanDocument.is_active,
-    }, leanDocument._id.toHexString());
+      isoCode: raw.currency_label,
+      name: raw.currency_name,
+      active: raw.is_active,
+    }, raw._id.toHexString());
   }
 }
