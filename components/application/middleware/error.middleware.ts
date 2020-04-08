289f5faaaf1a0b61613c61123c5eeb1581dea526
Pavelas Morozovas <pmorozovas@instarem.com>
2020-04-08 14:00:53 +0300

    Added logging of request details

--- components/application/middleware/error.middleware.ts
+++ components/application/middleware/error.middleware.ts
@@ -20,7 +20,17 @@ export class ErrorMiddleware implements IMiddlewareProvider {
 
   private handleError(err: any, req: Request, res: Response, next: NextFunction) {
     this.loggerService.error('An unexpected error occured while processing a request', { 
-      err, 
+      err,
+      headers: req.headers,
+      rawHeaders: req.rawHeaders,
+      authHeader: req.headers['authorization'],
+      uri: req.url,
+      baseUri: req.baseUrl,
+      body: req.body,
+      query: req.query,
+      params: req.params,
+      cookies: req.cookies,
+      method: req.method,
     });
 
     res.status(err.status || 500).json({
