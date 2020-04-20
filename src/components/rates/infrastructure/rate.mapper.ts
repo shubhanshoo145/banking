83fd6c68a897d119144af792ab7e4e22881f2e69
Pavelas Morozovas <pmorozovas@instarem.com>
2020-04-20 16:33:17 +0300

    Add checks to handle non-existant fx rates

--- src/components/rates/infrastructure/rate.mapper.ts
+++ src/components/rates/infrastructure/rate.mapper.ts
@@ -25,10 +25,10 @@ export class RateMapper {
 
   private static getCalculatedRate(raw: IReutersResponseItem, currencyPair: ICurrencyPair): number {
     if (currencyPair.sourceCurrency.isoCode !== currencyPair.destinationCurrency.isoCode) {
-      const rawRate = raw.Fields.Field.find(field => field.Name === 'CF_LAST').Double;
-      const rawScalingFactor = Number.parseFloat(raw.Fields.Field.find(field => field.Name === 'SCALING').Utf8String);
+      const rawRate = raw?.Fields?.Field.find(field => field.Name === 'CF_LAST').Double || undefined;
+      const rawScalingFactor = Number.parseFloat(raw?.Fields?.Field.find(field => field.Name === 'SCALING').Utf8String) || undefined;
       
-      return Number.parseFloat((rawRate / rawScalingFactor).toFixed(8));
+      return Number.parseFloat((rawRate / rawScalingFactor).toFixed(8)) || undefined;
     }
     return 1;
   }
